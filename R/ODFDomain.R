549acb111eaf037937a560550f38b5c9790274f3
Tutuchan <pierre.formont@gmail.com>
2016-05-16 20:48:55 +0200

    Handle geom fields

--- R/ODFDomain.R
+++ R/ODFDomain.R
@@ -6,24 +6,22 @@ ODFDomain <- R6::R6Class("ODFDomain",
                             domain = NULL,
                             facets = NULL,
                             n_datasets = NULL,
-                            slug = NULL,
                             sortables = NULL,
-                            initialize = function(slug){
-                              self$slug <- slug
-                              self$domain <- domains()$domains[domains()$slugs == slug]
+                            themes = NULL,
+                            initialize = function(domain){
+                              self$domain <- domain
                               self$n_datasets <- get_datasets(domain = self$domain)$data$nhits
                               self$facets <- datasets_facets()
                               self$sortables <- datasets_sortables()
+                              self$themes <- private$get_themes()
                             },
                             search = function(nrows = NULL, refine = NULL, exclude = NULL, sort = NULL, q = NULL, lang = NULL) {
                               listDatasets <- get_datasets(domain = self$domain, nrows, refine, exclude, sort, q, lang)$data$datasets
                               cat(paste(length(listDatasets), "datasets found ..."), "\n\n")
-                              for (i in 1:length(listDatasets)){
-                                dataset <- listDatasets[[i]]
-                                odfd <- ODFDataset$new(self$domain, dataset$datasetid)
-                                print(odfd)
-                                if (i < length(listDatasets)) cat("\n\n")
-                              }
+                              listDatasets <- lapply(listDatasets, function(dataset){
+                                ODFDataset$new(self$domain, dataset$datasetid)
+                              })
+                              listDatasets
                             },
 
                             print = function() {
@@ -31,8 +29,16 @@ ODFDomain <- R6::R6Class("ODFDomain",
                               cat("--------------------------------------------------------------------\n")
                               cat(paste("Domain:", self$domain, "\n"))
                               cat(paste("Number of datasets:", self$n_datasets, "\n"))
-                              cat(paste("Facets:", paste(self$facets, collapse = ", "), "\n"))
-                              cat(paste("Sortables:", paste(self$sortables, collapse = ", "), "\n"))
+                              cat(paste("Themes:\n  -", paste(self$themes, collapse = "\n  - "), "\n"))
                               cat("--------------------------------------------------------------------\n")
                             }
-                          ))
+                          ),
+                         private = list(
+                           get_themes = function(){
+                             lapply(get_datasets(domain = self$domain, nrows = self$n_datasets)$data$datasets, function(dataset) {
+                               dataset$metas$theme
+                             }) %>%
+                               unlist %>%
+                               unique %>%
+                               sort
+                           }))
--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,7 @@
 # Get content domain url
 get_domain_url <- function(domain, endpoint){
   stopifnot(domain %in% domains()$domains)
-  paste0("http://", get_base_url(domain), ".fr/api/", endpoint, "/1.0/")
+  paste0(get_base_url(domain), "/api/", endpoint, "/1.0/")
 }
 
 # Get datasets data
@@ -47,14 +47,32 @@ get_records <- function(domain, id, nrows, refine, exclude, sort, q, lang, geofi
   res <- jsonlite::fromJSON(url, simplifyVector = FALSE, flatten = FALSE) %$%
     records
 
-  res <- if (length(res) > 0) res %>%
-    purrr::transpose() %$%
-    fields %>%
-    purrr::transpose() %>%
-    lapply(unlist) %>%
-    dplyr::tbl_df() else dplyr::data_frame()
+  tidy_geom_xy <- function(x) {
+    lapply(x, function(xx) list(x = xx[[1]], y = xx[[2]]))
+  }
 
-  list(data = res, url = url)
+  tidy_geom <- function(x) {
+    lapply(x, function(xx) {
+      setNames(purrr::transpose(xx$coordinates[[1]]), c("x", "y")) %>%
+        purrr::transpose() %>%
+        dplyr::bind_rows()
+    })
+  }
+
+  out <- if (length(res) > 0) {
+    fields <- res %>%
+      purrr::transpose() %$%
+      fields %>%
+      purrr::transpose()
+
+    if ("geom" %in% names(fields)) fields$geom <- tidy_geom(fields$geom)
+    if ("geom_x_y" %in% names(fields)) fields$geom_x_y <- tidy_geom_xy(fields$geom_x_y)
+
+    c(fields[!names(fields) %in% c("geom", "geom_x_y")] %>% lapply(unlist), fields[names(fields) %in% c("geom", "geom_x_y")]) %>%
+      dplyr::tbl_df()
+  } else dplyr::data_frame()
+
+  list(data = out, url = url)
 }
 
 add_parameters_to_url <- function(url, nrows = NULL, refine = NULL, exclude = NULL, sort = NULL, q = NULL,
@@ -68,62 +86,82 @@ add_parameters_to_url <- function(url, nrows = NULL, refine = NULL, exclude = NU
           is.null(geofilter.distance),
           is.null(geofilter.polygon))) return(url) else additional_url <- c()
 
-  # Handle nrows
-  if (!is.null(nrows)) additional_url <- c(additional_url, rows = nrows)
+          # Handle nrows
+          if (!is.null(nrows)) additional_url <- c(additional_url, rows = nrows)
 
-  # Handle refine
-  if (!is.null(refine)) for (i in 1:length(refine)) {
-    facet = names(refine)[i]
-    val <- refine[[i]]
-    names(val) = paste0("refine.", facet)
-    additional_url <- c(additional_url, facet = facet, val)
-  }
+          # Handle refine
+          if (!is.null(refine)) for (i in 1:length(refine)) {
+            facet = names(refine)[i]
+            val <- refine[[i]]
+            names(val) = paste0("refine.", facet)
+            additional_url <- c(additional_url, facet = facet, val)
+          }
 
-  # Handle exclude
-  if (!is.null(exclude)) for (i in 1:length(exclude)) {
-    facet = names(exclude)[i]
-    val <- exclude[[i]]
-    names(val) = paste0("exclude.", facet)
-    additional_url <- c(additional_url, facet = facet, val)
-  }
+          # Handle exclude
+          if (!is.null(exclude)) for (i in 1:length(exclude)) {
+            facet = names(exclude)[i]
+            val <- exclude[[i]]
+            names(val) = paste0("exclude.", facet)
+            additional_url <- c(additional_url, facet = facet, val)
+          }
 
-  # Handle sort
-  if (!is.null(sort)) additional_url <- c(additional_url, sort = sort)
+          # Handle sort
+          if (!is.null(sort)) additional_url <- c(additional_url, sort = sort)
 
-  # Handle q
-  if (!is.null(q)) additional_url <- c(additional_url, q = q)
+          # Handle q
+          if (!is.null(q)) additional_url <- c(additional_url, q = q)
 
-  # Handle geofilter.distance
-  if (!is.null(geofilter.distance)) additional_url <- c(additional_url, geofilter.distance = paste(geofilter.distance, collapse = ","))
+          # Handle geofilter.distance
+          if (!is.null(geofilter.distance)) additional_url <- c(additional_url, geofilter.distance = paste(geofilter.distance, collapse = ","))
 
-  # Handle geofilter.polygon
-  if (!is.null(geofilter.polygon)) {
-    geofilter.polygon <- geofilter.polygon %>%
-      unite(polygon, lat, lon, sep = ",") %>%
-      mutate(polygon = paste0("(", polygon, ")")) %$%
-      polygon %>%
-      paste(collapse = ",")
-    additional_url <- c(additional_url, geofilter.polygon = geofilter.polygon)
-  }
-  url <- paste0(url, "?", paste(names(additional_url), additional_url, sep = "=", collapse = "&"))
-  print(url)
-  url
+          # Handle geofilter.polygon
+          if (!is.null(geofilter.polygon)) {
+            geofilter.polygon <- geofilter.polygon %>%
+              unite(polygon, lat, lon, sep = ",") %>%
+              mutate(polygon = paste0("(", polygon, ")")) %$%
+              polygon %>%
+              paste(collapse = ",")
+            additional_url <- c(additional_url, geofilter.polygon = geofilter.polygon)
+          }
+          url <- paste0(url, "?", paste(names(additional_url), additional_url, sep = "=", collapse = "&"))
+          print(url)
+          url
 
 }
 
 
 # Constants
 domains <- function(){
-  dplyr::data_frame(domains = c("iledefrance", "paris", "ratp", "hauts-de-seine"),
-                    slugs = c("idf", "paris", "ratp", "92"),
-                    base_urls = c("data", "opendata", "data", "opendata"))
+  dplyr::data_frame(domains = c("ratp",
+                                "iledefrance",
+                                "infogreffe",
+                                "toulouse",
+                                "star",
+                                "issy",
+                                "stif",
+                                "paris",
+                                "04",
+                                "62",
+                                "92",
+                                "enesr"),
+                    base_urls = c("http://data.ratp.fr",
+                                  "http://data.iledefrance.fr",
+                                  "http://datainfogreffe.fr",
+                                  "https://data.toulouse-metropole.fr",
+                                  "https://data.explore.star.fr",
+                                  "http://data.issy.com",
+                                  "http://opendata.stif.info",
+                                  "http://opendata.paris.fr",
+                                  "http://tourisme04.opendatasoft.com",
+                                  "http://tourisme62.opendatasoft.com",
+                                  "https://opendata.hauts-de-seine.fr",
+                                  "http://data.enseignementsup-recherche.gouv.fr"))
 }
 
 get_base_url <- function(domain){
   domains() %>%
-    dplyr::filter(domains == domain) %>%
-    tidyr::unite(url, base_urls, domains, sep = ".") %$%
-    url
+    dplyr::filter(domains == domain) %$%
+    base_urls
 }
 
 datasets_facets <- function(){
