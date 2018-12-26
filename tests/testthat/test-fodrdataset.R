330c38a132011f20eb07f5f32eabe31245bfdb56
denrou <deroussel@gmail.com>
2018-12-26 16:36:54 +0100

    :bug: some data have a geo_shape specification with a different name

--- tests/testthat/test-fodrdataset.R
+++ tests/testthat/test-fodrdataset.R
@@ -1,11 +1,13 @@
 context("test-fodrdataset")
 
-test_that("fodr_dataset works") {
+test_that("fodr_dataset works", {
   expect_is(fodr_dataset("ods", "correspondance-code-insee-code-postal"), "FODRDataset")
-}
+})
 
 test_that("get_records works", {
-  dts <- fodr_dataset("ods", "correspondance-code-insee-code-postal")
-  expect_is(df <- dts$get_records(nrows = 10), "tbl_df")
+  dts1 <- fodr_dataset("ods", "correspondance-code-insee-code-postal")
+  dts2 <- fodr_dataset("ods", "geoflar-departements")
+  expect_is(df <- dts1$get_records(nrows = 10), "tbl_df")
   expect_equal(nrow(df), 10)
+  expect_true("geo_shape" %in% names(dts2$get_records(nrow = 1)))
 })
